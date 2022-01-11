8c14832631284817d07257063b5d0d1122c6b428
Nathakorn Chaninthanadecha <nathakorn.tonch@gmail.com>
2022-01-12 06:18:36 +0700

    Add allowUndefined: true for dotenv

--- babel.config.js
+++ babel.config.js
@@ -3,11 +3,14 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'module:react-native-dotenv',
-      {
-        moduleName: '@env',
-        path: '.env.local',
-      },
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env.local',
+          allowUndefined: true,
+        },
+      ],
     ],
   }
 }
