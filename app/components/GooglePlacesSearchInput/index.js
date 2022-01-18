562eb0f37a2cd8bec03cba130fc52238a8e5df3e
Nathakorn Chaninthanadecha <nathakorn.tonch@gmail.com>
2022-01-18 13:44:05 +0700

    Change styling of component

--- app/components/GooglePlacesSearchInput/index.js
+++ app/components/GooglePlacesSearchInput/index.js
@@ -1,13 +1,26 @@
-// Customization autocomplete input style
-// Lib: https://github.com/FaridSafi/react-native-google-places-autocomplete
-// Expo API Ref: https://docs.expo.dev/versions/v44.0.0/react-native/view/
-
+import React from 'react'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 import { GOOGLE_MAP_API_KEY } from '@env'
 
 import tw from '../../../lib/tailwind'
 import { View } from 'react-native'
 
+// Customization autocomplete input style
+// Lib: https://github.com/FaridSafi/react-native-google-places-autocomplete#styling
+// Expo API Ref: https://docs.expo.dev/versions/v44.0.0/react-native/view/
+//
+// container	object (View)
+// textInputContainer	object (View style)
+// textInput	object (style)
+// listView	object (ListView style)
+// row	object (View style)
+// loader	object (View style)
+// description	object (Text style)
+// predefinedPlacesDescription	object (Text style)
+// separator	object (View style)
+// poweredContainer	object (View style)
+// powered	object (Image style)
+//
 const styles = {
   container: { flex: 0 }, // Tailwindcss issue
   textInput: tw.style('mb-0 h-12 rounded text-base', {
@@ -31,16 +44,16 @@ const styles = {
 
 const GooglePlacesSearchInput = () => {
   return (
-    <View style={tw`flex-col`}>
+    <View>
       <GooglePlacesAutocomplete
         styles={styles}
         placeholder={'Going to Mega Bang Na?'}
-        minLength={2}
         autoFocus={false}
         query={{
           key: GOOGLE_MAP_API_KEY,
           language: 'en', // TODO: implement user language later
         }}
+        minLength={2}
         nearbyPlacesAPI="GooglePlacesSearch"
         debounce={400}
       />
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -7,7 +7,7 @@ import GooglePlacesSearchInput from '../components/GooglePlacesSearchInput'
 const HomeScreen = () => {
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
-      <View style={tw`p-7`}>
+      <View style={tw`p-7 h-full`}>
         <Image
           style={{
             width: 100,
